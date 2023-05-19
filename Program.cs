c72232072a508361ef8e257d929f6b35e38a9a2b
tkaranamswathi <[swathitk1999@gmail.com]git config --global user.name [tkaranamswathi]>
2023-05-19 17:32:40 +0530

    [swathi] adding welcome message

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace UC1ATTENDANCE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int IS_PRESENT = 1;
+            Console.WriteLine("welcome to employeewagecompute");
+            Random random = new Random();
+            int randomInput = random.Next(0, 2);// 0 or 1
+            if (IS_PRESENT == randomInput)
+            {
+                Console.WriteLine("employee is present");
+            }
+            else
+            {
+                Console.WriteLine("employee is absent");
+
+            }
+        }
+    }
+}
+        }
+    }
+}

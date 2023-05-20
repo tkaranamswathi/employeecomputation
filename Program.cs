c1b3c660ec348bcbce521ebe192ad69559edcfb5
tkaranamswathi <[swathitk1999@gmail.com]git config --global user.name [tkaranamswathi]>
2023-05-20 17:14:29 +0530

    [swathi] adding welcome message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UC3PARTTIME
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

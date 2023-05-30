21a00e53cef0250780e03d6147213556d9b01d2a
tkaranamswathi <[swathitk1999@gmail.com]git config --global user.name [tkaranamswathi]>
2023-05-30 12:07:38 +0530

    [swathi] adding welcome message

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace UC5SWITCH
+{
+    internal class Program
+    {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to switchcase");
+  
+             int emphrs = 0;
+             int empwage = 0;
+             Random random = new Random();
+             int empcheck = random.Next(0, 3);
+             switch (empcheck)
+             {
+                case IS_PART_TIME:
+                    emphrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    emphrs = 8;
+                    break;
+                default:
+                    emphrs = 0;
+                    break;
+             }
+            empwage = emphrs + EMP_RATE_PER_HOUR;
+            Console.WriteLine("emp wage : " + empwage);
+        }
+    }
+}

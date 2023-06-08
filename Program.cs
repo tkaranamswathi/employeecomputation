1363b9b27d7ac90dcad94a1a052966422933f8b0
tkaranamswathi <[swathitk1999@gmail.com]git config --global user.name [tkaranamswathi]>
2023-06-08 17:32:18 +0530

    [adding welcome message]

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿namespace UC8MULTIPLECOMPANIES
+{
+    internal class Program
+    {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public static int ComputeEmpwage(string company, int empRatePerHour,int numOfWorkingDays,int maxHourPerMonth)
+        {
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+            while (totalEmpHrs <= maxHourPerMonth && totalWorkingDays < numOfWorkingDays)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day#:" + totalWorkingDays + " Emp Hrs :" + empHrs);
+            }
+            int totalEmpWage = totalEmpHrs * empRatePerHour;
+            Console.WriteLine("Total Emp Wage for company :" + company +" is: " + totalEmpWage);
+            return totalEmpWage;
+        }
+        static void Main(string[] args)
+        {
+            ComputeEmpwage("DMart",20,2,10);
+            ComputeEmpwage("Reliance", 10, 4, 20);
+        }
+    }
+
+}
+        

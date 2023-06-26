79b36609016676b9cbd25e5fffcc1ea2490d96ef
tkaranamswathi <[swathitk1999@gmail.com]git config --global user.name [tkaranamswathi]>
2023-06-26 12:07:42 +0530

    swathi

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace UC9SAVETOTALWAGE
+{
+ 
+    class program
+    {
+        static void Main(string[] args)
+        {
+            EmpWageBuilderObject dMart = new EmpWageBuilderObject("DMart",20,2,10);
+            EmpWageBuilderObject reliance = new EmpWageBuilderObject("Reliance",10,4,20);
+            dMart.computeEmpWage();
+            Console.WriteLine(dMart.ToString());
+            reliance.computeEmpWage();
+            Console.WriteLine(dMart.ToString());
+        }
+    }
+}
+
+
+    

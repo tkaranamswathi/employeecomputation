2df902969a19b9c04d43726ca49aeb245418edc1
tkaranamswathi <[swathitk1999@gmail.com]git config --global user.name [tkaranamswathi]>
2023-06-26 12:41:48 +0530

    swathi

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+namespace UC10SUPPORTEMPWAGE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+            empWageBuilder.addCompanyEmpWage("DMart", 20, 2, 10);
+            empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
+            empWageBuilder.computeEmpWage();
+        }
+    }
+}
